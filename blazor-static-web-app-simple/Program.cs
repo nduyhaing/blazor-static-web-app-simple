030da8b1a8eb6a66c43e7f3da00efd60b8927499
dhn <hai.nguyenduy@elca.vn>
2022-10-17 16:08:00 +0700

    Add project files.

---
+++ blazor-static-web-app-simple/Program.cs
@@ -0,0 +1,11 @@
+using blazor_static_web_app_simple;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+await builder.Build().RunAsync();
